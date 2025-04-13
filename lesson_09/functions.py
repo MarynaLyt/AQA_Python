3a8444ea07069a458a7aef37bb0cd7c4acb157f7
Maryna Lytvynenko <maryna.lytvynenko@paysera.net>
2025-04-13 21:59:55 +0300

    first part homework_09

---
+++ lesson_09/functions.py
@@ -0,0 +1,31 @@
+def calculates_sum_of_numbers(a, b) -> int:
+    """
+    The function that calculates the sum of two numbers
+    :return:
+    """
+    return a + b
+
+
+def arithmetic_mean_of_numbers(a, b) -> float:
+    """
+    The function that calculates the arithmetic mean of a list of numbers
+    :param a: srr
+    :param b: str
+    :return: arithmetic of entered numbers
+    """
+    arithmetic_of_numbers = (a + b) / 2
+    return arithmetic_of_numbers
+
+
+def longest_word_in_the_list(row: str) -> str:
+    """
+    The function that takes a list of words and returns the longest word in the list.
+    :param row: str
+    :return: str
+    """
+    words = row.split()  # Розбиваємо речення на список слів
+    longest = ""
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+    return longest
